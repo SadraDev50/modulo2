0c9fe81c64545f71e510b62f5b1d2f4fe52f4dfc
SandraDev50 <sosandracosta@gmail.com>
2022-02-22 21:31:13 -0300

    Exercicio do CPF

--- exercicio.js
+++ exercicio.js
@@ -1,25 +1,25 @@
 const input = require("readline-sync");
-const nome = input.question("Digite seu nome: ");
+// const nome = input.question("Digite seu nome: ");
 
-var nota1 = Number(input.question("Digite a sua primeira nota : "));
-var nota2 = Number(input.question("Digite sua segunda nota : "));
-var nota3 = Number(input.question("Digite sua terceira nota : "));
-var nota4 = Number(input.question("Digite sua quarta nota : "));
-var nota5 = Number(input.question("Digite sua quinta nota : "));
+// var nota1 = Number(input.question("Digite a sua primeira nota : "));
+// var nota2 = Number(input.question("Digite sua segunda nota : "));
+// var nota3 = Number(input.question("Digite sua terceira nota : "));
+// var nota4 = Number(input.question("Digite sua quarta nota : "));
+// var nota5 = Number(input.question("Digite sua quinta nota : "));
 
-var soma = parseFloat(nota1 + nota2 + nota3 + nota4 + nota5);
-var media = soma / 5;
+// var soma = parseFloat(nota1 + nota2 + nota3 + nota4 + nota5);
+// var media = soma / 5;
 
-if (media <= 3.5) {
-  console.log(nome + " a sua media foi " + media + " " + "Você foi reprovado");
-} else if (media >= 3.6 && media <= 6.9) {
-  console.log(
-    nome + "a sua media foi " + media + " " + "Você está em recuperação");
-} else if (media >= 7 && media <= 10) {
-  console.log(nome + " a sua média foi " + media + " " + "Você foi aprovado");
-} else {
-  console.log("media >10 inválida");
-}
+// if (media <= 3.5) {
+//   console.log(nome + " a sua media foi " + media + " " + "Você foi reprovado");
+// } else if (media >= 3.6 && media <= 6.9) {
+//   console.log(
+//     nome + "a sua media foi " + media + " " + "Você está em recuperação");
+// } else if (media >= 7 && media <= 10) {
+//   console.log(nome + " a sua média foi " + media + " " + "Você foi aprovado");
+// } else {
+//   console.log("media >10 inválida");
+// }
 
 //let disciplina = "A turma do back do Senac é Massa";
 
@@ -28,4 +28,8 @@ if (media <= 3.5) {
 // let frase = "Eu sou aluna da turma do back"
 // let fraseInvertida = frase.split("").reverse().join("")
 // console.log("A frase é "+ frase + "A frase Invertida" + fraseInvertida)
-   
+  
+
+ 
+const cpf = input.question("digite seu cpf: ");
+      console.log(cpf) 
